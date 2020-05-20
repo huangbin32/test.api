35e4ee67ee22c3989d8acfaba34d430bc33b75bf
WG-huangbin32 <github>
2020-05-20 15:37:46 +0800

    课程模块

--- config/basic_config.py
+++ config/basic_config.py
@@ -1,9 +1,9 @@
 #coding=utf-8
 class ConfigInit:
-    url = 'https://baconapi.lpi-china.com'
-    login_url = 'https://baconapi.lpi-china.com'
-    sendaddr_name = '1171499448@qq.com'
-    sendaddr_pswd = 'expfsnlwjyecjddj'
+    url = 'xxxx'
+    login_url = 'xxxx'
+    sendaddr_name = 'xxxx'
+    sendaddr_pswd = 'xxxx'
     data_filename = 'data.xls'
     mongo_ip_port = 'ip'
     mongo_user = 'root'
